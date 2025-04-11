2173029abd3faf27f0a564e92cd7bbd52919bb89
master <oleg.cherniy@gmail.com>
2025-04-11 22:55:18 +0300

    YOLO TensorRT converter was added!

--- modelhub_client_trt/__init__.py
+++ modelhub_client_trt/__init__.py
@@ -3,7 +3,7 @@ The modelhub_client_trt module.
 """
 from .modelhub_client_trt import ModelHubTrt, _TRT_VERSION_STR, get_device_name
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
 __all__ = (
     '__version__',
